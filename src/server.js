79c5ed5de91f57c173dd251b1bde8eb00e6a4283
Royce119 <madcoder911@gmail.com>
2024-01-02 03:21:44 +0200

    fixing build

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8000;
 async function startServer() {
   await mongoConnect();
   app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
   });
 }
 startServer();
